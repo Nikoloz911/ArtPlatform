887ddd00570ad618de2d1eb63918f48d5ff67fb4
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-07-11 13:23:52 +0400

    Finsihed Subscription Service added Last APIs

--- src/SubscriptionService/Helpers/SubscriptionMappingProfile.cs
+++ src/SubscriptionService/Helpers/SubscriptionMappingProfile.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
+using SubscriptionService.DTOs;
+using SubscriptionService.Models;
 
 namespace SubscriptionService.Helpers;
 public class SubscriptionMappingProfile : Profile
 {
     public SubscriptionMappingProfile()
     {
-        
+        CreateMap<Subscription, SubscriptionDTO>();
+        CreateMap<Subscription, FollowerDTO>();
     }
 }
