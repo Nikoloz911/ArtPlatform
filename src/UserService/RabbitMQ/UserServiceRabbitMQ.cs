f546db7213054bb5ded13cdae1d8c3eafa759ef6
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-07-16 15:39:12 +0400

    Fixed Namespace

--- src/UserService/RabbitMQ/UserServiceRabbitMQ.cs
+++ src/UserService/RabbitMQ/UserServiceRabbitMQ.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using UserService.Data;
 using UserService.Models;
 
+namespace UserService.RabbitMQ;
 public class UserServiceRabbitMQ
 {
     private readonly IServiceProvider _serviceProvider;
